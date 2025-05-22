397ee0310727364eb9c160919aea29c61db33f21
suradatnb <bondnuy007@me.com>
2025-05-22 12:02:16 +0700

    Mongodb Cloud

--- app/config.py
+++ app/config.py
@@ -1,11 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # โหลดค่าจาก .env
+load_dotenv()
 
-MONGO_URI = os.getenv(
-    "MONGO_URI", 
-    "mongodb+srv://bondnuy007:xWt0w8D0NjvbvlvM@isansoftdb.coskaoq.mongodb.net/?retryWrites=true&w=majority&appName=IsanSoftDB"
-)
-
-DB_NAME = os.getenv("DB_NAME", "quotation_db")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://bondnuy007:xWt0w8D0NjvbvlvM@isansoftdb.coskaoq.mongodb.net/?retryWrites=true&w=majority&appName=IsanSoftDB")
+DB_NAME = os.getenv("loadair_db", "quotation_db")
