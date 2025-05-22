ae73672240ff136d2aea4fd8258d3713bd466153
suradatnb <bondnuy007@me.com>
2025-05-22 11:57:46 +0700

    Upconfig

--- app/config.py
+++ app/config.py
@@ -1,7 +1,11 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()  # โหลดค่าจาก .env
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
-DB_NAME = os.getenv("loadair_db", "quotation_db")
+MONGO_URI = os.getenv(
+    "MONGO_URI", 
+    "mongodb+srv://bondnuy007:xWt0w8D0NjvbvlvM@isansoftdb.coskaoq.mongodb.net/?retryWrites=true&w=majority&appName=IsanSoftDB"
+)
+
+DB_NAME = os.getenv("DB_NAME", "quotation_db")
